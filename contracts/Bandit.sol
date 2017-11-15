c4c28125f5f023b3679f1c9318a95d377fa041d3
Richard Gill <altaudiolabs@gmail.com>
2017-11-15 13:42:33 +0000

    save

--- contracts/Bandit.sol
+++ contracts/Bandit.sol
@@ -5,17 +5,9 @@ contract Bandit {
   uint256 win = 1;
   uint256 lose = 0;
 
-  function pullLever(uint userNumber) public returns (uint256) {
+  function randomNumber() public constant returns (uint256) {
     uint randomNumber = uint(block.blockhash(block.number-1))%10 + 1;
-
-    if (userNumber == randomNumber) {
-      return win;
-    }
-    return lose;
-  }
-
-  function getCounter() public constant returns (uint256) {
-    return win;
+    return randomNumber;
   }
 
 
@@ -26,5 +18,3 @@ contract Bandit {
     }
   }
 }
-
-// Bandit.deployed().then(function(instance){return instance.pullLever(1);});
