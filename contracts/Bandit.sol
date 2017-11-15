a6f7648def55dd9e0b2bc229e272976478e988b9
Richard Gill <altaudiolabs@gmail.com>
2017-11-15 13:09:18 +0000

    Changes % to 1/10

--- contracts/Bandit.sol
+++ contracts/Bandit.sol
@@ -2,7 +2,7 @@ pragma solidity ^0.4.4;
 
 contract Bandit {
   function pullLever(uint userNumber) public returns (uint) {
-    uint randomNumber = uint(block.blockhash(block.number-1))%2 + 1;
+    uint randomNumber = uint(block.blockhash(block.number-1))%10 + 1;
 
     if (userNumber == randomNumber) {
       return 1;
