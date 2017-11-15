58d63c47241b77cdcccbbfef099300ecb0273526
Richard Gill <altaudiolabs@gmail.com>
2017-11-15 14:02:40 +0000

    save

--- contracts/Bandit.sol
+++ contracts/Bandit.sol
@@ -4,7 +4,7 @@ contract Bandit {
   address owner = msg.sender;
   uint win = 1;
   uint lose = 0;
-  uint nonce = 1;
+  uint nonce = 200;
 
   function getRandomNumber() public constant returns (uint) {
     nonce++;
