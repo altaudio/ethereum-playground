0d2e8d6469e8e53bdc19ec611fdd3e26394a9592
Richard Gill <altaudiolabs@gmail.com>
2017-11-15 13:43:33 +0000

    save

--- contracts/Bandit.sol
+++ contracts/Bandit.sol
@@ -5,7 +5,7 @@ contract Bandit {
   uint256 win = 1;
   uint256 lose = 0;
 
-  function randomNumber() public constant returns (uint256) {
+  function getRandomNumber() public constant returns (uint256) {
     uint randomNumber = uint(block.blockhash(block.number-1))%10 + 1;
     return randomNumber;
   }
